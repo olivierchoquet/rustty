4d618796bd0048ab8e9235e4a34d972dd241b715
Choquet Olivier <olivier.choquet@vinci.be>
2026-01-30 13:37:11 +0100

    ok

--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -1,5 +1,7 @@
+// Rust cherche automatiquement un fichier mod.rs dans le dossier components
+// on peut donc définir les sous-modules ici pour les rendre accessibles depuis l'extérieur
 pub mod table;   // Rend le fichier table.rs accessible
-pub mod sidebar; // Rend le fichier sidebar.rs accessible
-pub mod forms;   // Rend le fichier forms.rs accessible
-pub mod actions; // Rend le fichier actions.rs accessible
-pub mod brand;
+pub mod sidebar; 
+pub mod forms;   
+pub mod actions; 
+pub mod brand; 
